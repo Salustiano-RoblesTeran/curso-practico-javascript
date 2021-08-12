0ea2da01e8a6116cf39931d4541be59f69eeea6f
Salustiano Robles Teran <saluroblesteran@gmail.com>
2021-08-11 23:00:53 -0300

    Creo la primer funcion

--- figuras.js
+++ figuras.js
@@ -1,1 +1,24 @@
-console.log("Hello world");
+//Codigo del cuadrado
+
+//const ladoCuadrado = 2;
+let area;
+let perimetro;
+
+const perimetroCuadrado = (lado) => {
+    return lado * 4;
+}
+
+perimetro = perimetroCuadrado(3);
+
+const areaCuadrado = (lado) => {
+    return lado * lado;
+}
+
+
+area = areaCuadrado(3);
+
+//console.log("Los lados del cuadrado miden: " + ladoCuadrado + "cm");
+
+console.log(`El perimetro del cuadrado es: ${perimetro} cm`);
+
+console.log(`El area del cuadrado es: ${area} cm^2`);
